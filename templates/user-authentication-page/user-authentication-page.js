643ea34d6f6802ab87326ba5f12d66cd25123872
chanchal2 <patilchanchal01@gmail.com>
2022-04-16 18:24:33 +0530

    changes remaning in update

--- templates/user-authentication-page/user-authentication-page.js
+++ templates/user-authentication-page/user-authentication-page.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     console.log("hii");
     getCalculatedTag();
+    selectrolename();
     $("#datatablesCalculatedTag1").on('click', '.editValues', function () {
         // var today = new Date();
         // var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
@@ -9,42 +10,78 @@ $(document).ready(function () {
         var userName = $(this).closest('tr').find('td:eq(0)').text();
         var userRole = $(this).closest('tr').find('td:eq(1)').text();
         var empName = $(this).closest('tr').find('td:eq(2)').text();
-        var empEmailID = $(this).closest('tr').find('td:eq(3)').text();
-        var empEmailNotification = $(this).closest('tr').find('td:eq(4)').text();
-        var empMobileNo = $(this).closest('tr').find('td:eq(5)').text();
-        var userType = $(this).closest('tr').find('td:eq(9)').text();
+        var empMiddleName = $(this).closest('tr').find('td:eq(3)').text();
+        var empLastName = $(this).closest('tr').find('td:eq(4)').text();
+        var empEmailID = $(this).closest('tr').find('td:eq(5)').text();
+        var empEmailNotification = $(this).closest('tr').find('td:eq(6)').text();
+        var empMobileNo = $(this).closest('tr').find('td:eq(7)').text();
+        var empEmailStatus = $(this).closest('tr').find('td:eq(10)').text();
+        var userType = $(this).closest('tr').find('td:eq(11)').text();
         document.getElementById("tagNamep").value = userName;
-        $("#tagValuep option[value='"+ userRole +"']").attr("selected", "selected");
-        $("#empEmailnotificaton option[value='"+ empEmailNotification +"']").attr("selected", "selected");
-        $("#userType option[value='"+ userType +"']").attr("selected", "selected");
+        $("#tagValuep option[value='" + userRole + "']").attr("selected", "selected");
+        $("#empEmailnotificaton option[value='" + empEmailNotification + "']").attr("selected", "selected");
+        $("#empstatus option[value='" + empEmailStatus + "']").attr("selected", "selected");
+        $("#userType option[value='" + userType + "']").attr("selected", "selected");
         // document.getElementById("tagValuep").value = tagValue;
         document.getElementById("empName").value = empName;
+        document.getElementById("empMiddleName").value = empMiddleName;
+        document.getElementById("empLastName").value = empLastName;
         document.getElementById("empEmailId").value = empEmailID;
         document.getElementById("empMobileNo").value = empMobileNo;
         $('#updateRow').show();
     });
-    
+
 
     $("#userType").on("change", function () {
         // console.log("from", event.target.value);
         if ($('#userType').val() == 'Local') {
-            $('#localPassword').show();  
-        }else{
+            $('#localPassword').show();
+        } else {
             $('#localPassword').hide();
         }
-        
-      });
-      $("#addUserType").on("change", function () {
+
+    });
+    $("#addUserType").on("change", function () {
         // console.log("from", event.target.value);
         if ($('#addUserType').val() == 'Local') {
-            $('#addlocalPassword').show();  
-        }else{
+            $('#addlocalPassword').show();
+        } else {
             $('#addlocalPassword').hide();
         }
-        
-      });
+
+    });
 
 });
+// code for role dropdown
+function selectrolename() {
+    $.ajax({
+        method: "GET",
+        url: "http://localhost:8090/EMSPNC/getroledata",
+
+    }).done(function (values) {
+        for (const abc of values) {
+            $('#addUserRole').append($(document.createElement('option')).prop({
+                value: abc.role_id,
+                text: abc.role_name
+            }))
+            $('#tagValuep').append($(document.createElement('option')).prop({
+                value: abc.role_id,
+                text: abc.role_name
+            }))
+        }
+    });
+}
+
+
+// code for see password 
+function seepass() {
+    var pass = document.getElementById("addempPassword");
+    if (pass.type === "password") {
+        pass.type = "text";
+    } else {
+        pass.type = "password";
+    }
+}
 
 // add model
 function usermodel() {
@@ -78,41 +115,45 @@ function saveTable() {
     var addUser = $('#inputColumnName').val();
     var addUserRole = $("#addUserRole").val();
     var addEmployeeName = $("#addUserName").val();
+    var addEmployeeMiddeleName = $("#addUserMiddleName").val();
+    var addEmployeeLasteName = $("#addUserLastName").val();
     var addEmailId = $("#addEmailId").val();
     var addEmailNotification = $("#addEmailNotification").val();
     var addMobile = $("#addMobile").val();
+    var addEmailStatus = $("#addstatus").val();
     var addUserType = $("#addUserType").val();
     var addPassword = $("#addempPassword").val();
     var tabledata = document.getElementById("datatablebody");
-    
+
     if (addUserType == 'Local') {
         $('#addempPassword').show();
-        
+
     }
     // var today = new Date();
     // var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
     // var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     // const numberRow = $(tabledata).find('tr');
     // console.log('numberRow', $(numberRow).find('#inputColumnName').val());
-    if (addUser == "" || addUserRole == "" || addEmployeeName == "" || addEmailId == "" || addEmailNotification == "" || addMobile == "" || addUserType == "") {
+    if (addUser == "" || addUserRole == "" || addEmployeeName == "" || addEmployeeMiddeleName == "" || addEmployeeLasteName == "" || addEmailId == "" || addEmailNotification == "" || addMobile == "" || addUserType == "") {
         document.getElementById("eTableerroe").innerText = 'Please Enter Values.....';
     }
     else {
-        var dataObj ={
-            "username":addUser,
-            "role": addUserRole,
-                "role_id": 10,
-                "employee_name": addEmployeeName,
-                "email_id": addEmailId,
-                "email_notification": addEmailNotification,
-                "password": addPassword,
-                "mobile_no":addMobile,
-                "login_time": "2022-03-15T15:00:00",
-                "logout_time": "2022-03-15T15:00:00",
-                "status": "Active",
-                
-                "user_type": addUserType
-                }
+        var dataObj = {
+            "user_name": addUser,
+            "role_id": addUserRole,
+            "employee_firstname": addEmployeeName,
+            "employee_middlename": addEmployeeMiddeleName,
+            "employee_lastname": addEmployeeLasteName,
+            "email_id": addEmailId,
+            "email_notification": addEmailNotification,
+            "password": addPassword,
+            "mobile_no": addMobile,
+            "login_time": "2022-03-15T15:00:00",
+            "logout_time": "2022-03-15T15:00:00",
+            "status": addEmailStatus,
+
+            "user_type": addUserType
+        }
         console.log(dataObj, "messa");
         var rowdata1 = JSON.stringify(dataObj)
         console.log('array', rowdata1);
@@ -125,10 +166,6 @@ function saveTable() {
             url: "http://localhost:8090/EMSPNC/insertuserdetails",
             data: rowdata1,
             success: function (msg) {
-
-
-
-
                 var msg1 = msg.status;
                 if (msg1 == 'Record Inserted Sucessfully') {
                     // document.getElementById("eTable").innerText = 'Manualtag inserted successfully';
@@ -137,6 +174,8 @@ function saveTable() {
                     document.getElementById("inputColumnName").value = "";
                     document.getElementById("addUserRole").value = "";
                     document.getElementById("addUserName").value = "";
+                    document.getElementById("addUserMiddleName").value = "";
+                    document.getElementById("addUserLastName").value = "";
                     document.getElementById("addEmailId").value = "";
                     document.getElementById("addEmailNotification").value = "";
                     document.getElementById("addMobile").value = "";
@@ -144,7 +183,7 @@ function saveTable() {
                     $('#AdduserRow').hide()
 
                 }
-                if (msg == 'Username Already Exists!') {
+                if (msg1 == "Username Already Exists!") {
                     alert("This User is Already Exists")
                 }
                 else {
@@ -165,49 +204,30 @@ function getCalculatedTag() {
         method: "GET",
         url: "http://localhost:8090/EMSPNC/getalldata",
     }).done(function (data) {
-        fetchgetCalculatedTag(data)
+        console.log(data, "hjhji");
+        var tabledata = data;
+        var student = '';
+        student += '<tr>';
+        for (const val of tabledata) {
+            student += '<td>' + val['user_name'] + '</td>';
+            student += '<td>' + val['role_id'] + '</td>';
+            student += '<td>' + val['employee_firstname'] + '</td>';
+            student += '<td>' + val['employee_middlename'] + '</td>';
+            student += '<td>' + val['employee_lastname'] + '</td>';
+            student += '<td>' + val['email_id'] + '</td>';
+            student += '<td>' + val['email_notification'] + '</td>';
+            student += '<td>' + val['mobile_no'] + '</td>';
+            student += '<td>' + val['login_time'] + '</td>';
+            student += '<td>' + val['logout_time'] + '</td>';
+            student += '<td>' + val['status'] + '</td>';
+            student += '<td>' + val['user_type'] + '</td>';
+            student += '<td><input class="editValues btn btn-primary"  type="button" value="Edit"</input></td>';
+            student += '<td><input type="checkbox" class="delusermanual" name="delBox" value="Edit Row" /></td>';
+            student += '</tr>';
+        }
+        document.getElementById("bodytablesCalculatedTag").innerHTML = student;
 
     })
-        // .fail(function () {
-        //     var dummyjson = [{
-        //         "username": "priyanka@iocl",
-        // "role": "systemadmin",
-        // "role_id": 1,
-        // "employee_name": "Priyanka Wagh",
-        // "email_id": "priyankawagh31@gmail.com",
-        // "email_notification": true,
-        // "password": "priyanka123",
-        // "mobile_no": 8999.0,
-        // "login_time": "2022-03-15T15:00:00",
-        // "logout_time": "2022-03-15T15:00:00",
-        // "status": "Active",
-        // "token": 123,
-        // "user_type": "L-Dap"               
-        //     }]
-        //     fetchgetCalculatedTag(dummyjson)
-        // });
-}
-function fetchgetCalculatedTag(data) {
-    console.log(data,"hjhji");
-    var tabledata = data;
-    var student = '';
-    student += '<tr>';
-    for (const val of tabledata) {
-        student += '<td>' + val['username'] + '</td>';
-        student += '<td>' + val['role'] + '</td>';
-        student += '<td>' + val['employee_name'] + '</td>';
-        student += '<td>' + val['email_id'] + '</td>';
-        student += '<td>' + val['email_notification'] + '</td>';
-        student += '<td>' + val['mobile_no'] + '</td>';
-        student += '<td>' + val['login_time'] + '</td>';
-        student += '<td>' + val['logout_time'] + '</td>';
-        student += '<td>' + val['status'] + '</td>'; 
-        student += '<td>' + val['user_type'] + '</td>'; 
-        student += '<td><input class="editValues btn btn-primary"  type="button" value="Edit"</input></td>';
-        student += '<td><input type="checkbox" class="delusermanual" name="delBox" value="Edit Row" /></td>';
-        student += '</tr>';
-    }
-    document.getElementById("bodytablesCalculatedTag").innerHTML = student;
 }
 
 // Update Data from update model
@@ -215,21 +235,28 @@ $('#updateRowBtn').click(function () {
     var tagNamep = $('#tagNamep').val();
     var tagValuep = $('#tagValuep').val();
     var empName = $('#empName').val();
+    var empMiddleName = $('#empMiddleName').val();
+    var empLastName = $('#empLastName').val();
     var empEmailId = $('#empEmailId').val();
     var empEmailnotificaton = $('#empEmailnotificaton').val();
+    var empEmailStatus = $("#empstatus").val();
     var empMobileNo = $('#empMobileNo').val();
     var userType = $('#userType').val();
-    
+
     if (userType == 'Local') {
         $('#localPassword').show();
         var password = $('#empPassword').val();
-        var updateRow = { "username": tagNamep, "role": tagValuep, "employee_name": empName, 'email_id': empEmailId,
-     'email_notification': empEmailnotificaton ,'mobile_no': empMobileNo,'user_type': userType ,'password':password}
-    }else{
-        var updateRow = { "username": tagNamep, "role": tagValuep, "employee_name": empName, 'email_id': empEmailId,
-     'email_notification': empEmailnotificaton ,'mobile_no': empMobileNo,'user_type': userType ,'password':' '}
+        var updateRow = {
+            "user_name": tagNamep, "role": tagValuep, "employee_firstname": empName, "employee_middlename": empMiddleName, "employee_lastname": empLastName, 'email_id': empEmailId,
+            'email_notification': empEmailnotificaton, 'mobile_no': empMobileNo,'status':empEmailStatus, 'user_type': userType, 'password': password
+        }
+    } else {
+        var updateRow = {
+            "user_name": tagNamep, "role": tagValuep, "employee_firstname": empName,  "employee_middlename": empMiddleName, "employee_lastname": empLastName, 'email_id': empEmailId,
+            'email_notification': empEmailnotificaton, 'mobile_no': empMobileNo, 'status':empEmailStatus, 'user_type': userType, 'password': ' '
+        }
     }
-    
+
     $('#updateRow').hide();
     $.ajax({
         headers: {
@@ -252,15 +279,6 @@ $('#updateRowBtn').click(function () {
         }
     });
 });
-
-//select all delete
-// $('#deleteuserdata').click(function (event) {
-//     if (this.checked) {
-//         $(':checkbox').prop('checked', true);
-//     } else {
-//         $(':checkbox').prop('checked', false);
-//     }
-// });
 
 // delete data 
 function userdeletedata() {
